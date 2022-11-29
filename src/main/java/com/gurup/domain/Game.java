c3b0e48bb1eaa30243f4338fc66892d02bb8bf5a
Omer <Omer@DESKTOP-S9J3J7B.mshome.net>
2022-11-30 00:36:47 +0300

    Matches MVC better than the initial code.

--- src/main/java/com/gurup/domain/Game.java
+++ src/main/java/com/gurup/domain/Game.java
@@ -1,5 +1,41 @@
 package com.gurup.domain;
 
-public class Game {
+import java.awt.Color;
+import java.util.Timer;
 
+import javax.swing.SwingUtilities;
+
+import com.gurup.controller.MovementController;
+import com.gurup.ui.ScreenMaker;
+import com.gurup.ui.gamescreen.RunningModeScreen;
+
+public class Game {
+	
+	private static ScreenMaker screenMaker;
+	private static Player player;
+	private static MovementController movementController;
+	private static RunningModeScreen runningModeScreen;
+	
+	public static void main(String[] args) {
+		screenMaker = new ScreenMaker();
+		player = new Player(Color.blue, 20, 20, 450, 450);
+		runningModeScreen = screenMaker.createScreen(player, movementController);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				screenMaker.createAndShowGUI(runningModeScreen);
+			}
+		});
+		movementController = new MovementController(player, runningModeScreen);
+		
+		// running timer task as daemon thread
+		Timer timer = new Timer(true);
+		System.out.println(Thread.currentThread().getName() + " TimerTask started");
+		// cancel after sometime
+		try {
+			Thread.sleep(100000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		timer.cancel();
+	}
 }
--- src/main/java/com/gurup/ui/ScreenMaker.java
+++ src/main/java/com/gurup/ui/ScreenMaker.java
@@ -1,5 +1,31 @@
 package com.gurup.ui;
 
-public class ScreenMaker {
+import java.awt.Color;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import com.gurup.controller.MovementController;
+import com.gurup.domain.Player;
+import com.gurup.ui.gamescreen.RunningModeScreen;
+
+public class ScreenMaker {
+	public RunningModeScreen createScreen(Player player, MovementController movementController) {
+		RunningModeScreen runningModeScreen = new RunningModeScreen(player, movementController);
+		return runningModeScreen;
+	}
+	public RunningModeScreen createAndShowGUI(RunningModeScreen runningModeScreen) {
+
+		System.out.println(Thread.currentThread().getName() + " Created GUI " + SwingUtilities.isEventDispatchThread());
+
+		JFrame f = new JFrame("OYUN");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Player player = new Player(Color.blue, 20, 20, 450, 450);
+		f.add(runningModeScreen);
+
+		f.pack();
+		f.setVisible(true);
+		return runningModeScreen;
+	}
 }
