559578ff861d4765d9b81651498117f5da41399a
eeray19 <eery19@ku.edu.tr>
2022-11-29 20:47:27 +0300

    Removed unused lines and changed missed ball to player

--- src/main/java/com/gurup/timerbasedanimation/Oyun.java
+++ src/main/java/com/gurup/timerbasedanimation/Oyun.java
@@ -2,52 +2,45 @@ package com.gurup.timerbasedanimation;
 
 import java.awt.Color;
 import java.util.Timer;
-import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class Oyun {
-	
-	
-    public static void main(String args[]) {
-    	
-       
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI(); 
-            }
-        });
-        
-        
-            
-        //running timer task as daemon thread
-        Timer timer = new Timer(true);
-        System.out.println(Thread.currentThread().getName()+" TimerTask started");
-        //cancel after sometime
-        try {
-            Thread.sleep(100000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        timer.cancel();
-    }
-    
-    
-    private static void createAndShowGUI() {
-    	
-    	System.out.println(Thread.currentThread().getName() +" Created GUI "+
-    	        SwingUtilities.isEventDispatchThread());
-    	
-    	JFrame f = new JFrame("Swing Animation Demo");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-       
-        Player ball2 = new Player(Color.blue, 20, 20, 450, 450);
-    	f.add(new Building(ball2));
-    	
-    	f.pack();
-    	f.setVisible(true);
-    	
-    }
+
+	public static void main(String args[]) {
+
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+
+		// running timer task as daemon thread
+		Timer timer = new Timer(true);
+		System.out.println(Thread.currentThread().getName() + " TimerTask started");
+		// cancel after sometime
+		try {
+			Thread.sleep(100000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		timer.cancel();
+	}
+
+	private static void createAndShowGUI() {
+
+		System.out.println(Thread.currentThread().getName() + " Created GUI " + SwingUtilities.isEventDispatchThread());
+
+		JFrame f = new JFrame("Swing Animation Demo");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Player player = new Player(Color.blue, 20, 20, 450, 450);
+		f.add(new Building(player));
+
+		f.pack();
+		f.setVisible(true);
+
+	}
 
 }
