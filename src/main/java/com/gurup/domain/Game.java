baaf5a59d4740e75e74327dbc84d8a00bcf520b0
aligebesce <aligebesce0@gmail.com>
2022-12-08 19:45:19 +0300

    Login & Register Added

--- src/main/java/com/gurup/domain/Game.java
+++ src/main/java/com/gurup/domain/Game.java
@@ -13,38 +13,40 @@ import com.gurup.domain.account.entity.AccountOperationResults;
 import com.gurup.domain.account.manager.AccountManager;
 import com.gurup.domain.room.Room;
 import com.gurup.ui.ScreenMaker;
+import com.gurup.ui.gamescreen.LoginScreen;
 import com.gurup.ui.gamescreen.RunningModeScreen;
 
 public class Game {
-	
+
 	private static ScreenMaker screenMaker;
 	private static Player player;
 	private static Room room;
 	private static MovementController movementController;
 	private static KeyClickController keyClickController;
 	private static RunningModeScreen runningModeScreen;
+	private static LoginScreen loginScreen;
 	private static final int PLAYER_SIZE = 25;;
-	private static Scanner input;
 	private static AccountManager accountManager;
 	private static String session;
 
-	
-	
 	public static void main(String[] args) throws Exception {
-		input = new Scanner(System.in);
+		screenMaker = new ScreenMaker();
 		accountManager = new AccountManager();
-		if(mainScreen()) {
-			screenMaker = new ScreenMaker();
+		loginScreen = screenMaker.createMainModeScreen();
+		boolean isLoginSuccesful = mainScreen();
+
+		if (isLoginSuccesful) {
 			player = new Player(Color.blue, 50, 50,
 					Toolkit.getDefaultToolkit().getScreenSize().width - 100 + PLAYER_SIZE,
 					Toolkit.getDefaultToolkit().getScreenSize().height - 175 + PLAYER_SIZE, PLAYER_SIZE);
 			room = new Room("Student Center", 50, 50, Toolkit.getDefaultToolkit().getScreenSize().width - 100,
 					Toolkit.getDefaultToolkit().getScreenSize().height - 175, player);
 
-			runningModeScreen = screenMaker.createScreen(player, movementController, keyClickController, room);
+			runningModeScreen = screenMaker.createRunningModeScreen(player, movementController, keyClickController,
+					room);
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
-					screenMaker.createAndShowGUI(runningModeScreen);
+					screenMaker.showRunningModeGUI(runningModeScreen);
 				}
 			});
 			movementController = new MovementController(player, runningModeScreen);
@@ -61,39 +63,37 @@ public class Game {
 			timer.cancel();
 		}
 	}
+
 	private static boolean mainScreen() throws Exception {
-		System.out.println("Please write your username:");
-		String username = input.nextLine();
-		System.out.println("Please write your password:");
-		String password1 = input.nextLine();
-		System.out.println("Please write your password again:");
-		String password2 = input.nextLine();
-		System.out.println("Please write your mail:");
-		String mail = input.nextLine();
-		System.out.println("Please choose your action:");
-		System.out.println("Register");
-		System.out.println("Login");
-		String choice = input.nextLine();
-		if(choice.equals("Quit")) return false;
-		if(choice.equals("Register")) {
-			
+		boolean registerFlag = loginScreen.isRegisterPressed();
+		boolean loginFlag = loginScreen.isLoginPressed();
+
+		do {
+			registerFlag = loginScreen.isRegisterPressed();
+			loginFlag = loginScreen.isLoginPressed();
+			Thread.sleep(10);
+
+		} while (!registerFlag && !loginFlag);
+
+		String username = loginScreen.getEnteredUsermame();
+		String password1 = loginScreen.getEnteredPassword();
+		String password2 = loginScreen.getEnteredPassword();
+		String mail = loginScreen.getEnteredMail();
+
+		if (registerFlag) {
+			loginScreen.setRegisterPressed(false);
 			AccountOperationResults res = accountManager.createAccount(username, password1, password2, mail);
-			if(res.equals(AccountOperationResults.SUCCESS)) {
-				System.out.println(res);
-			}else {
-				System.out.println(res);
-			}
-		}else if(choice.equals("Login")) {
+			System.out.println("Register Result: " + res.toString());
+		} else if (loginFlag) {
+			loginScreen.setLoginPressed(false);
 			AccountOperationResults res = accountManager.loginAccount(username, password1, password2, mail);
-			if(res.equals(AccountOperationResults.SUCCESS)) {
-				System.out.println(res);
+			System.out.println("Login Result: " + res.toString());
+			if (res.equals(AccountOperationResults.SUCCESS)) {
 				session = username;
 				return true;
-			}else {
+			} else {
 				System.out.println(res);
 			}
-		}else {
-			System.out.println("Failed try again!");
 		}
 		return mainScreen();
 	}
