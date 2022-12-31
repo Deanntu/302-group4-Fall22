ad9a0f8b38b5236b1a87d73b7df9f4d0b474275d
Omer <Omer@DESKTOP-S9J3J7B.mshome.net>
2022-12-31 12:54:11 +0300

    Right wall test works

--- src/main/java/com/gurup/test/BottleUsePowerUpTest.java
+++ src/main/java/com/gurup/test/BottleUsePowerUpTest.java
@@ -92,7 +92,23 @@ public class BottleUsePowerUpTest {
 	}
 	@Test
 	public void nearRightWall() {
-		
+		for (int i = player.getxLimit(); i > player.getxLimit()-100; i--) {
+			player.setX(i);
+			ThrownBottlePowerUp bottle = ThrownBottlePowerUp.getInstance(player);
+			bottle.setUsable(true);
+			bottle.usePowerUp("up");
+			assertTrue(bottle.getX() == player.getX() && bottle.getY() == player.getY() - 100);
+			bottle.setUsable(true);
+			bottle.usePowerUp("down");
+			assertTrue(bottle.getX() == player.getX() && bottle.getY() == player.getY() + 100);
+			bottle.setUsable(true);
+			bottle.usePowerUp("left");
+			assertTrue(bottle.getX() == player.getX() - 100 && bottle.getY() == player.getY());
+			bottle.setUsable(true);
+			bottle.usePowerUp("right");
+			assertTrue(bottle.getX() == player.getxLimit() && bottle.getY() == player.getY());
+			ThrownBottlePowerUp.setNull();
+		}
 	}
 	@Test
 	public void nearTopWall() {
