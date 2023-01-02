6b93ed0a88c05a3c3bf90a1658bc82809cec709a
Deanntu <tugrademirel19@ku.edu.tr>
2023-01-02 06:12:18 +0300

    WIP PlayerDatabaseGameSaver

--- src/main/java/com/gurup/domain/saver/PlayerDatabaseGameSaver.java
+++ src/main/java/com/gurup/domain/saver/PlayerDatabaseGameSaver.java
@@ -1,11 +1,24 @@
 package com.gurup.domain.saver;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import com.gurup.domain.DatabaseRequirements;
 import com.gurup.domain.Game;
 import com.gurup.domain.Player;
-
+import com.gurup.domain.powerups.BottlePowerUp;
+import com.gurup.domain.powerups.VestPowerUp;
 
 public class PlayerDatabaseGameSaver {
+	private Boolean isProtected;
+	private int remainingTime;
+	private int remainingLife;
+	private int xLocation;
+	private int yLocation;
+	private int bottlecount;
+	private int vestcount;
+
 	public PlayerDatabaseGameSaver() {
 		try {
 			Class.forName(DatabaseRequirements.driver.getValue());
@@ -14,19 +27,37 @@ public class PlayerDatabaseGameSaver {
 			e.printStackTrace();
 		}
 	}
+
 	public GameSaverOperationResults savePlayer(String username, Player player) throws Exception {
-		//TODO Create table according to the requirements below.
-		player.getIsProtected();
-		player.getRemainingTime();
-		player.getRemainingLife();
-		player.getX();
-		player.getY();
-		Game.getBag().getPowerUps();		
-		
-		return trySavePlayer();
+		// TODO Create table according to the requirements below.
+		isProtected = player.getIsProtected();
+		remainingTime = player.getRemainingTime();
+		remainingLife = player.getRemainingLife();
+		xLocation = player.getX();
+		yLocation = player.getY();
+		bottlecount = Game.getBag().getPowerUps().get(BottlePowerUp.getInstance(player));
+		vestcount = Game.getBag().getPowerUps().get(VestPowerUp.getInstance(player));
+
+		return trySavePlayer(username);
 	}
-	private GameSaverOperationResults trySavePlayer() {
+
+	private GameSaverOperationResults trySavePlayer(String username) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		Connection connection = DriverManager.getConnection(DatabaseRequirements.url.getValue(),
+				DatabaseRequirements.username.getValue(), DatabaseRequirements.password.getValue());
+		String sql = "INSERT INTO public.player(isprotected, remainingtime, remaininglife, xlocation, ylocation, bottle, vest, username)	VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setBoolean(1, isProtected);
+		statement.setInt(2, remainingTime);
+		statement.setInt(3, remainingLife);
+		statement.setInt(4, xLocation);
+		statement.setInt(5, yLocation);
+		statement.setInt(6, bottlecount);
+		statement.setInt(7, vestcount);
+		statement.setString(8, username);
+		int affected = statement.executeUpdate();
+
+		connection.close();
+		return affected > 0 ? GameSaverOperationResults.SUCCESS : GameSaverOperationResults.FAIL;
 	}
 }
