bb9eb5234ab4a6568b11d2ff85ddbb58f4b0a896
yakupenesguven <yakupenes.guven@gmail.com>
2023-01-06 21:19:27 +0300

    aliens backend started

--- src/main/java/com/gurup/domain/aliens/BlindAlien.java
+++ src/main/java/com/gurup/domain/aliens/BlindAlien.java
@@ -1,7 +1,10 @@
 package com.gurup.domain.aliens;
 
 import java.awt.Rectangle;
+import java.util.Random;
 
+import com.gurup.domain.Player;
+import com.gurup.domain.powerups.ThrownBottlePowerUp;
 import com.gurup.domain.room.Room;
 
 public class BlindAlien implements Alien {
@@ -14,7 +17,10 @@ public class BlindAlien implements Alien {
 	private int xCurrent;
 	private int yCurrent;
 	private boolean isActive = false;
+	private Random random = new Random();
 
+	private Player player;
+	private ThrownBottlePowerUp thrownBottlePowerUp = ThrownBottlePowerUp.getInstance(player);
 	public BlindAlien(int xStart, int yStart, int xLen, int yLen) {
 		this.xStart = xStart;
 		this.yStart = yStart;
@@ -24,6 +30,8 @@ public class BlindAlien implements Alien {
 
 	public BlindAlien() {
 		// TODO Auto-generated constructor stub
+		randomMove();
+
 	}
 
 	public void moveRight() {
@@ -55,6 +63,34 @@ public class BlindAlien implements Alien {
 			this.yCurrent = (Room.getYLimit());
 		} else {
 			this.yCurrent = (this.yCurrent + 10);
+		}
+	}
+	public void randomMove() {
+		int direction = random.nextInt(4);
+		int counter = random.nextInt(4);
+		switch (direction) {
+			case 0:
+				while (counter!=0){
+					moveRight();
+					counter-=1;
+				}
+			case 1:
+				while (counter!=0){
+					moveLeft();
+					counter-=1;
+				}
+			case 2:
+				while (counter!=0){
+					moveUp();
+					counter-=1;
+				}
+			case 3:
+				while (counter!=0){
+					moveDown();
+					counter-=1;
+				}
+			default:
+				randomMove();
 		}
 	}
 
--- src/main/java/com/gurup/domain/aliens/ShooterAlien.java
+++ src/main/java/com/gurup/domain/aliens/ShooterAlien.java
@@ -2,6 +2,8 @@ package com.gurup.domain.aliens;
 
 import java.awt.Rectangle;
 
+import com.gurup.domain.Player;
+import com.gurup.domain.powerups.ThrownBottlePowerUp;
 import com.gurup.domain.room.Room;
 
 public class ShooterAlien implements Alien {
@@ -15,6 +17,10 @@ public class ShooterAlien implements Alien {
 	private int yCurrent;
 	private boolean isActive = false;
 
+	private int diff = 40;
+	private Player player;
+	private ThrownBottlePowerUp thrownBottlePowerUp = ThrownBottlePowerUp.getInstance(player);
+
 	public ShooterAlien(int xStart, int yStart, int xLen, int yLen) {
 		this.xStart = xStart;
 		this.yStart = yStart;
@@ -24,6 +30,27 @@ public class ShooterAlien implements Alien {
 
 	public ShooterAlien() {
 		// TODO Auto-generated constructor stub
+		shoot();
+	}
+
+	public void shoot(){
+		if(!player.isProtected()&& playerInsideRange()){
+			player.setLife(player.getLife() - 1);
+		}
+	}
+
+	public boolean playerInsideRange(){
+		int playerX = player.getXCurrent();
+		int playerY = player.getYCurrent();
+
+		int diffX = playerX - getXCurrent();
+		int diffY = playerY - getYCurrent();
+
+		if(diffX < diff  && diffY < diff){
+			return true;
+		}
+		return false;
+
 	}
 
 	public void moveRight() {
