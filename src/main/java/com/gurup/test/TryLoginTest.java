6ace5db637142ac62d8a9ec0a16f4c9756ab4077
eeray19 <eery19@ku.edu.tr>
2023-01-08 22:30:45 +0300

    Added password check

--- src/main/java/com/gurup/test/TryLoginTest.java
+++ src/main/java/com/gurup/test/TryLoginTest.java
@@ -16,11 +16,12 @@ public class TryLoginTest {
             Manager.delete("esma");
             Manager.createAccount("esma", "12345", "12345", "me@eeray.com");
             assertEquals(AccountOperationResults.EMPTY_FIELD, Manager.loginAccount("", "", "", ""));
-            assertEquals(AccountOperationResults.EMPTY_FIELD, Manager.loginAccount("", "123", "123", "me@eeray.com"));
-            assertEquals(AccountOperationResults.EMPTY_FIELD, Manager.loginAccount("esma", "123", "123", ""));
-            assertEquals(AccountOperationResults.WRONG_MAIL_FORMAT, Manager.loginAccount("esma", "123", "123", "a@acom"));
-            assertEquals(AccountOperationResults.WRONG_CREDENTIAL, Manager.loginAccount("esmaa", "123", "123", "me@eeray.com"));
-            assertEquals(AccountOperationResults.WRONG_CREDENTIAL, Manager.loginAccount("esma", "123", "123", "me@eeray.comm"));
+            assertEquals(AccountOperationResults.EMPTY_FIELD, Manager.loginAccount("", "12345", "12345", "me@eeray.com"));
+            assertEquals(AccountOperationResults.EMPTY_FIELD, Manager.loginAccount("esma", "12345", "12345", ""));
+            assertEquals(AccountOperationResults.WRONG_MAIL_FORMAT, Manager.loginAccount("esma", "12345", "12345", "a@acom"));
+            assertEquals(AccountOperationResults.WRONG_CREDENTIAL, Manager.loginAccount("esmaa", "12345", "12345", "me@eeray.com"));
+            assertEquals(AccountOperationResults.WRONG_CREDENTIAL, Manager.loginAccount("esma", "12345", "12345", "me@eeray.comm"));
+            assertEquals(AccountOperationResults.WRONG_CREDENTIAL, Manager.loginAccount("esma", "9", "9", "me@eeray.com"));
             assertEquals(AccountOperationResults.SUCCESS, Manager.loginAccount("esma", "12345", "12345", "me@eeray.com"));
         } catch (Exception e) {
             // TODO Auto-generated catch block
