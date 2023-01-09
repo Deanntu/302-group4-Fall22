d9526d36f2a7148e518cda9330003f40056a44f2
aligebesce <aligebesce0@gmail.com>
2023-01-10 02:35:31 +0300

    revert to feature 200

--- src/main/java/com/gurup/test/IsKeyFoundTest.java
+++ src/main/java/com/gurup/test/IsKeyFoundTest.java
@@ -2,6 +2,7 @@ package com.gurup.test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.awt.Rectangle;
 import java.awt.Toolkit;
